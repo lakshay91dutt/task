38c123a59cff9eefb7eb6cedcf81816ff048433b
lakshay91dutt <lakshay91dutt@gmail.com>
2019-01-30 18:16:41 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -5,31 +5,37 @@ import './css/style.css';
 import './css/queries.css';
 import './vendors/css/grid.css'
 class App extends Component {
-  constructor (props){
-    super (props);
+    constructor (props){
+        super (props);
 
-    this.state = {
-      firstName: '',
-      lastName: '',
-      emailId: '',
-      mobile: '',
-      doj: '',
-      address: '',
-      designation: '',
-      department: '',
-      ctc: '',
-      status: ''
+        this.state = {
+            empId: '',
+            firstName: '',
+            lastName: '',
+            emailId: '',
+            mobile: '',
+            doj: '',
+            address: '',
+            designation: '',
+            department: '',
+            ctc: '',
+            status: ''
+        }
+        this.json = {}
     }
-    this.handleSubmit = (event) => {
+    loadJSON = () => {
+        const jsonFileData = window.localStorage.getItem()
+    }
+    handleSubmit = (event) => {
       event.preventDefault();
       document.write('<p>' + JSON.stringify(this.state) +'</p>');
     }
-    this.handleInputChange = (event) => {
+    handleInputChange = (event) => {
       event.preventDefault()
+      console.log(event.target.name, event.target.value);
       this.setState({
         [event.target.name]: event.target.value
       })
-    }
   };
   render(){
     return (
@@ -40,13 +46,23 @@ class App extends Component {
 
         <section className = "section-form">
           <form className = "contact-form" onSubmit = {this.handleSubmit} >
+          {/* -------------Employee ID----------------- */}
+          <div className = "row">
+                  <div className = "col span-1-of-3">
+                      <label>Employee ID</label>
+                  </div>
+                  <div className = "col span-2-of-3">
+                      <input type= "text" name= "empId" placeholder= "Unique Employee id" onChange = {this.handleInputChange} defaultValue = {null}></input>
+                  </div>
+            </div>
+            <br></br>
           {/* -------------Name----------------- */}
             <div className = "row">
                   <div className = "col span-1-of-3">
                       <label>First Name</label>
                   </div>
                   <div className = "col span-2-of-3">
-                      <input type= "text" name= "firstName" placeholder= "First Name" onChange = {this.handleInputChange}></input>
+                      <input type= "text" name= "firstName" placeholder= "First Name" onChange = {this.handleInputChange} defaultValue = {null}></input>
                   </div>
             </div>
             <br></br>
@@ -56,7 +72,7 @@ class App extends Component {
                       <label>Last Name</label>
                   </div>
                   <div className = "col span-2-of-3">
-                      <input type= "text" name= "lastName" placeholder= "Last Name" onChange = {this.handleInputChange}></input>
+                      <input type= "text" name= "lastName" placeholder= "Last Name" onChange = {this.handleInputChange} defaultValue = {null}></input>
                   </div>
             </div>
             <br></br>
@@ -76,7 +92,7 @@ class App extends Component {
                       <label>Mobile</label>
                   </div>
                   <div className = "col span-2-of-3">
-                      <input type= "text" name= "mobile" placeholder= "Mobile Number" onChange = {this.handleInputChange}></input>
+                      <input type= "text" name= "mobile" placeholder= "Mobile Number" onChange = {this.handleInputChange}  defaultValue = {null}></input>
                   </div>
             </div>
             <br></br>
@@ -86,7 +102,7 @@ class App extends Component {
                       <label>Date Of Joining</label>
                   </div>
                   <div className = "col span-2-of-3">
-                      <input type= "date" name= "doj" onChange = {this.handleInputChange}></input>
+                      <input type= "date" name= "doj" onChange = {this.handleInputChange}  defaultValue = {null}></input>
                   </div>
             </div>
             <br></br>
@@ -96,7 +112,7 @@ class App extends Component {
                       <label>Address</label>
                   </div>
                   <div className = "col span-2-of-3">
-                      <textarea type= "text" name= "address" placeholder= "Residential Address" onChange = {this.handleInputChange}></textarea>
+                      <textarea type= "text" name= "address" placeholder= "Residential Address" onChange = {this.handleInputChange}  defaultValue = {null}></textarea>
                   </div>
             </div>
             <br></br>
@@ -106,15 +122,40 @@ class App extends Component {
                     <label>Designation</label>
                 </div>
                 <div className="col span-2-of-3">
-                    <select name="designation">
-                        <option value="back-end">Back-End Developer</option>
-                        <option value="devop">DevOps</option>
-                        <option value="front-end" selected>Front-End Developer</option>
-                        <option value="ios">IOS Developer</option>
-                        <option value="android">Android Developer</option>
-                        <option value="other">Other</option>
+                    <select name="designation" onChange = {this.handleInputChange}>
+                        <option value="back-end" onChange = {this.handleInputChange}>Back-End Developer</option>
+                        <option value="devop" onChange = {this.handleInputChange}>DevOps</option>
+                        <option value="front-end"  onChange = {this.handleInputChange} selected>Front-End Developer</option>
+                        <option value="ios" onChange = {this.handleInputChange}>IOS Developer</option>
+                        <option value="android"  onChange = {this.handleInputChange}>Android Developer</option>
+                        <option value="other" onChange = {this.handleInputChange}>Other</option>
                     </select>
                 </div>
+            </div>
+            <br></br>
+            {/* -------------Department----------------- */}
+            <div className="row">
+                <div className="col span-1-of-3">
+                    <label>Department</label>
+                </div>
+                <div className="col span-2-of-3">
+                    <select name="department">
+                        <option value="web development" onChange = {this.handleInputChange}>Web Development</option>
+                        <option value="devop" onChange = {this.handleInputChange}>DevOps</option>
+                        <option value="android development"  onChange = {this.handleInputChange} selected>Android Development</option>
+                        <option value="ios development" onChange = {this.handleInputChange}>IOS</option>
+                    </select>
+                </div>
+            </div>
+            <br></br>            
+            {/* -------------CTC----------------- */}
+            <div className = "row">
+                  <div className = "col span-1-of-3">
+                      <label>CTC</label>
+                  </div>
+                  <div className = "col span-2-of-3">
+                      <input type= "number" name= "ctc" placeholder= "Offered CTC (in Lacs per annum)" onChange = {this.handleInputChange} defaultValue = {null}></input>
+                  </div>
             </div>
             <br></br>
             {/* -------------Marital Status----------------- */}
@@ -124,9 +165,9 @@ class App extends Component {
                 </div>
                 <div className="col span-2-of-3">
                     <label>Married: </label>
-                    <input type="checkbox" name="status" value="married"/>
+                    <input type="checkbox" name="status" value="married" onChange = {this.handleInputChange}/>
                     <label>Single: </label>
-                    <input type="checkbox" name="status" value="Single"/>
+                    <input type="checkbox" name="status" value="Single"  onChange = {this.handleInputChange} checked/>
                 </div>
             </div>
           {/* -------------Buttons----------------- */}
